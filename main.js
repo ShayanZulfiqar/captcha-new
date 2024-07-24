598042c643f5acc07ded46ed6ed5707d5e8f5d5a
Shayan Zulfiqar <146065078+ShayanZulfiqar@users.noreply.github.com>
2024-07-24 06:37:12 +0400

    heo

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ window.onloadTurnstileCallback = function () {
         sitekey: '0x4AAAAAAAfqzm9_KK7B52xE',
         callback: function (token) {
             console.log(`Challenge Success ${token}`);
-            window.location.href = 'https://www.google.com';
+            // window.location.href = 'https://www.google.com';
         },
         'theme': 'light', // Optional: Adjust to match your design
     });
