c272936ce7f1a3a204f4325802ffe85c17dae729
Shayan Zulfiqar <146065078+ShayanZulfiqar@users.noreply.github.com>
2024-07-24 05:34:12 +0400

    shayan

--- main.js
+++ main.js
@@ -1,14 +1,5 @@
 
 
-window.onloadTurnstileCallback = function () {
-    turnstile.render('#myWithget', {
-        sitekey: '0x4AAAAAAAfqzm9_KK7B52xE',
-        callback: function(token) {
-            console.log(`Challenge Success ${token}`);
-        },
-    });
-};
-
 
 var verifyCallback_CF = function (response) {
     let cfForm = document.querySelector("#cfForm")
